f838e37144111565e5c96af80f8fc5048334b69c
NicholasT <nickt1325@gmail.com>
2023-06-07 15:36:02 +0400

    Task 2

---
+++ Assigment3/Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter a number: ");
+string input = Console.ReadLine();
+int num;
+int divider = 1;
+if (int.TryParse(input, out num))
+{
+    for (int i = 2; i <= num; i++)
+    {
+        if (num % i == 0) divider += 1;
+    }
+    Console.WriteLine("The number of dividers for " + num + " is " + divider);
+}
+else
+{
+    Console.WriteLine("Enter valid number");
+}

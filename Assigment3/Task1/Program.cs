82b1122ddb5a9b99c47729db73e3b6ef3a81d86b
NicholasT <nickt1325@gmail.com>
2023-06-07 15:24:42 +0400

    Assigment 3 task 1

---
+++ Assigment3/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter a number: ");
+string input = Console.ReadLine();
+int num;
+int divider = 1;
+if (int.TryParse(input, out num))
+{
+    if (num == 1 || num == 0) Console.WriteLine(num + " is not prime nor composite number");
+    else
+    {
+        for (int i = 2; i <= num; i++)
+        {
+            if (num % i == 0) divider += 1;
+        }
+        if (divider > 2) Console.WriteLine(num + " is composite number");
+        else Console.WriteLine(num + " is prime number");
+    }
+}
+else
+{
+    Console.WriteLine("Enter valid number");
+}

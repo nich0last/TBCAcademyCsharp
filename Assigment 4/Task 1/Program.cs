310510081b34bb4a26a8773e3c9c493d4bc685e3
NicholasT <nickt1325@gmail.com>
2023-06-09 16:31:34 +0400

    Assigment 4 First two tasks

---
+++ Assigment 4/Task 1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Please enter Array length: ");
+string arraylst = Console.ReadLine();
+int arrayl;
+
+if(int.TryParse(arraylst, out arrayl))
+{
+    int[] myarray = new int[arrayl];
+    for(int i = 0; i < arrayl; i++)
+    {
+        Console.Write("Enter value for element " + i + ":");
+        myarray[i] = int.Parse(Console.ReadLine());
+    }
+
+    for (int i = 0; i < arrayl - 1; i++)
+    {
+        for (int j = 0; j < arrayl - 1 - i; j++)
+        {
+            if (myarray[j] > myarray[j + 1])
+            {
+                int temp = (int)myarray[j];
+                myarray[j] = myarray[j + 1];
+                myarray[j + 1] = temp;
+            }
+        }
+    }
+
+    Console.WriteLine("Sorted Array:");
+    foreach (int i in myarray)
+    {
+        Console.Write(i + ":");
+    }
+}
+else
+{
+    Console.WriteLine("Please enter valid number");
+}
---
+++ Assigment 4/Task 2/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Please enter length for string array: ");
+string Ainput = Console.ReadLine();
+int ALength;
+
+if(int.TryParse(Ainput,out ALength))
+{
+    string[] mystring = new string[ALength];
+    for (int i = 0;i < ALength; i++)
+    {
+        Console.Write("Please enter value for string array element " +  i + ":");
+        mystring[i] = Console.ReadLine();
+    }
+    Console.Write("Please enter length for int array: ");
+    string Binput = Console.ReadLine();
+    int BLength;
+    if(int.TryParse(Binput,out BLength))
+    {
+        int[] myint = new int[BLength];
+        for (int i = 0; i < BLength; i++)
+        {
+            Console.Write("Please enter value for int array element " + i + ":");
+            myint[i] = int.Parse(Console.ReadLine());
+        }
+
+        int maxsize = Math.Max(ALength, BLength);
+
+        for (int i = 0; i < maxsize; i++)
+        {
+            if (i < ALength) { Console.Write(mystring[i]); }
+            if (i < BLength) { Console.Write(myint[i]); }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please enter valid number");
+    }
+}
+else
+{
+    Console.WriteLine("Please enter valid number");
+}

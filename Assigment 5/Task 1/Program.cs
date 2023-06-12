17355a469d3f640bf1dbdc58f4b7bb498810e482
NicholasT <nickt1325@gmail.com>
2023-06-12 18:09:03 +0400

    Assigment 5 Full

---
+++ Assigment 5/Task 1/Program.cs
@@ -0,0 +1,41 @@
+﻿int arraysize;
+do
+{
+    Console.WriteLine("Enter length for your array:");
+} while(!int.TryParse(Console.ReadLine(), out arraysize));
+
+int[] Array = new int[arraysize];
+
+for (int i = 0; i < arraysize; i++)
+{
+    do
+    {
+        Console.WriteLine($"Enter Value for array element {i}: ");
+    } while (!int.TryParse(Console.ReadLine(), out Array[i]));
+}
+
+int index;
+do
+{
+    Console.WriteLine("Enter Index to find Value");
+} while (!int.TryParse(Console.ReadLine(), out index) || index>=arraysize);
+
+int sum = getSumByIndex(Array, index);
+Console.WriteLine($"Sum of digits for your number is: {sum}");
+
+int getSumByIndex(int[] array, int index)
+{
+    return CalculateSumOfDigits(array[index]);
+}
+int CalculateSumOfDigits(int number)
+{
+    number = Math.Abs(number);//In Case of Negative Number
+    int sum = 0;
+    while (number != 0)
+    {
+        int digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+    return sum;
+}
---
+++ Assigment 5/Task 2/Program.cs
@@ -0,0 +1,43 @@
+﻿char[] mychar = GetCharArrayFromUser();
+
+Console.WriteLine("Please Enter Symbol to find");
+char symbol = Console.ReadKey().KeyChar;
+Console.WriteLine(); ;
+PrintResult(symbol, FindSymbol(mychar, symbol));
+
+
+char[] GetCharArrayFromUser()
+{
+    int arraysize;
+    do
+    {
+        Console.WriteLine("Enter Size for your Char Array");
+    }   while(!int.TryParse(Console.ReadLine(), out arraysize));
+
+    char[] array = new char[arraysize];
+    for(int i = 0; i < arraysize; i++)
+    {
+        Console.Write($"Enter Value for char array element {i}: ");
+        array[i] = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int FindSymbol(char[] mychar, char symbol)
+{
+    int Count = 0;
+    for(int i = 0;i < mychar.Length;i++)
+    {
+        if (mychar[i] == symbol)
+        {
+            Count++;
+        }
+    }
+    return Count;
+}
+
+void PrintResult(char symbol, int count)
+{
+    Console.WriteLine($"{symbol} was found in array {count} times");
+}

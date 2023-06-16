8b850b61607b76578234868848e2680e8fb7cf36
NicholasT <nickt1325@gmail.com>
2023-06-17 02:17:12 +0400

    Assigment 6 Task 1

---
+++ Assigment 6/Task 1/NameSpaceLibrary.cs
@@ -0,0 +1,100 @@
+﻿namespace LibrarySystem
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+    }
+
+    public class Library
+    {
+        private Book?[] _books;
+        private int _count;
+
+        public Library(int capacity)
+        {
+            _books = new Book[capacity];
+            _count = 0;
+        }
+
+        public Book? this[int index]
+        {
+            get { return _books[index]; }
+            set { _books[index] = value; }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+        }
+
+        public void AddBook(Book book)
+        {
+            if(_count<_books.Length)
+            {
+                _books[_count] = book;
+                _count++;
+            }
+            else
+            {
+                Console.WriteLine("The Library is full");
+            }
+        }
+
+        public void RemoveBook(int? index = null)
+        {
+            if(index == null)
+            {
+                Array.Clear(_books, 0, _books.Length);
+                _count = 0;
+            }
+            else
+            {
+                if(index >= 0 && index< _count)
+                {
+                    for (int i = index??0; i < _count - 1; i++)
+                    {
+                        _books[i] = _books[i + 1];
+                    }
+                    _count--;
+                    _books[_count] = null;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid index");
+                }
+            }
+        }
+        public Book? Find(string searchCriteria, string searchTerm)
+        {
+            return SearchHelper.FindBook(_books, searchCriteria, searchTerm);
+        }
+    }
+    public static class SearchHelper
+    {
+        public static Book? FindBook(Book?[] books,  string searchCriteria, string searchTerm)
+        {
+            switch(searchCriteria.ToLower())
+            {
+                case "author":
+                    return Array.Find(books, book => book?.Author == searchTerm);
+                case "title":
+                    return Array.Find(books, book => book?.Title == searchTerm);
+                case "year":
+                    if(int.TryParse(searchTerm, out int year) )
+                    return Array.Find(books, book => book?.Year == year);
+                    break;
+            }
+            return null;
+        }
+    }
+}
---
+++ Assigment 6/Task 1/Program.cs
@@ -0,0 +1,55 @@
+﻿using LibrarySystem;
+{
+    Library myLibrary = new Library(5);
+
+    myLibrary.AddBook(new Book("Lord Of The Rings", "J.R.R. Tolkien", 1954));
+    myLibrary.AddBook(new Book("Harry Potter", "J.K. Rowling", 1997));
+    myLibrary.AddBook(new Book("Data Tutashkhia", "Chabua Amirejibi", 1975));
+
+    //find book by author
+    Book? findBookByAuthor = myLibrary.Find("author", "J.K. Rowling");
+    if (findBookByAuthor != null)
+    {
+        Console.WriteLine("Book found: " + findBookByAuthor.Title);
+    }
+    else
+    {
+        Console.WriteLine("Book not found");
+    }
+
+    //Find Book By Title
+    Book? findBookByTitle = myLibrary.Find("title", "Data Tutashkhia");
+    if (findBookByTitle != null)
+    {
+        Console.WriteLine("Author of the book " + findBookByTitle.Title + " is " + findBookByTitle.Author);
+    }
+    else
+    {
+        Console.WriteLine("Book Not found");
+    }
+
+    //Find Book By year 
+    Book? findBookByYear = myLibrary.Find("year", "1954");
+    if (findBookByYear != null)
+    {
+        Console.WriteLine("Book found: " + findBookByYear.Title);
+    }
+    else
+    {
+        Console.WriteLine("Book not found.");
+    }
+
+    // Remove a book
+    myLibrary.RemoveBook(1);
+
+    // Print the remaining books in the library
+    Console.WriteLine("Books in the library:");
+    for (int i = 0; i < myLibrary.Count; i++)
+    {
+        Book? book = myLibrary[i];
+        if (book != null)
+        {
+            Console.WriteLine("Title: " + book.Title + ", Author: " + book.Author + ", Year: " + book.Year);
+        }
+    }
+}
